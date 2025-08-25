8247a8e350b7c25ef6bc44cc5cef5cc45378e4da
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-08-25 20:15:33 +0530

    Explore column layout, button ,state and uplifting of the state of parent composable from child composable

--- app/src/main/java/com/warmup/jetpackcomposemastery/MainActivity.kt
+++ app/src/main/java/com/warmup/jetpackcomposemastery/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.warmup.jetpackcomposemastery
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.warmup.jetpackcomposemastery.ui.theme.JetpackComposeMasteryTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +32,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeMasteryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    var counter by rememberSaveable { mutableIntStateOf(0) }
+                    Column(modifier = Modifier.fillMaxSize().padding(innerPadding), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text("Counter value is $counter")
+                        Spacer(modifier = Modifier.height(20.dp))
+                        ChildComponent {
+                            counter += 1
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +47,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeMasteryTheme {
-        Greeting("Android")
+fun ChildComponent(onIncrement:()-> Unit){
+    Button(onClick = onIncrement) {
+        Text("Increment")
     }
 }
